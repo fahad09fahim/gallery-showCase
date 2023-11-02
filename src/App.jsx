0b55665c9d96cf6af8fe174e5caea072ddfc61ad
unknown <anik.fahim07@gmail.com>
2023-11-02 21:04:24 +0600

    used React icon and dynamically set navbar elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { ImCheckboxChecked } from "react-icons/im";
 const App = () => {
   const [images, setImages] = useState([
     "/src/assets/images/image-1.webp",
@@ -15,8 +15,11 @@ const App = () => {
     "/src/assets/images/image-11.jpeg",
   ]);
 
+  // selected image state
   const [selected, setSelected] = useState([]);
-  console.log(selected.length);
+  // console.log(selected);
+
+  // This function is for checking image selection.
   const targetImage = (index) => {
     if (selected.includes(index)) {
       setSelected(selected.filter((item) => item !== index));
@@ -29,25 +32,38 @@ const App = () => {
     <div className="px-10 mb-10">
       <div className="shadow-xl p-5">
         <nav className="m-7 bg-slate-200 p-4 rounded-md">
-          {selected.length>0 ? (
-            <h1>{selected.length}</h1>
-          ):(
+          {selected.length > 0 ? (
+            <div className="flex justify-between">
+            <h1 className="text-xl flex items-center gap-2">
+              <ImCheckboxChecked /> {selected.length} { selected.length >1 ? "files": "file"} Selected
+            </h1>
+            <button>Delete { selected.length >1 ? "files": "file"}</button>
+            </div>
+          ) : (
             <h1 className="text-xl">Gallery</h1>
-          ) }
+          )}
         </nav>
         <div className="grid grid-cols-5 gap-4 mx-7 ">
           {images.map((img, index) => (
             <div
-              className={`border-2 border-gray-400 rounded p-5 relative group hover:block hover:cursor-pointer  ${
+              className={`border-2 border-gray-400 rounded p-5 relative group  hover:cursor-pointer  ${
                 index === 0 ? "col-span-2 row-span-2" : ""
               }`}
               key={index}
             >
-              <img src={img} alt="" className="max-w-full max-h-max" />
+              {/* conditionally handle when image is selected */}
+              <img
+                src={img}
+                alt=""
+                className={`max-w-full max-h-max ${
+                  selected.includes(index) ? "blur-sm" : ""
+                }`}
+              />
+
               <input
                 type="checkbox"
                 className=" hidden absolute top-2 left-2 cursor-pointer group-hover:block h-7 w-5 checked:block"
-                checked={selected.includes(index)}
+                // checked={selected.includes(index)}
                 onChange={() => targetImage(index)}
               />
             </div>
