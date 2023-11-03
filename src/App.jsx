2c926715d56d911ff6bdb676b8300c2bd39d61c5
unknown <anik.fahim07@gmail.com>
2023-11-03 10:31:25 +0600

    rearrange code,get read to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { ImCheckboxChecked } from "react-icons/im";
-// import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-// import { ReactSortable } from "react-sortablejs";
-
-
-
-
 
 
 const App = () => {
@@ -26,7 +20,6 @@ const App = () => {
   // selected image state
   const [selected, setSelected] = useState([]);
 
-
   // This function is for checking image selection.
   const targetImage = (index) => {
     if (selected.includes(index)) {
@@ -45,15 +38,14 @@ const App = () => {
     setSelected([]);
   };
 
-
-// ---------------------------------------------
-const handleImageReorder = (dragIndex, dropIndex) => {
-  const reorderedImages = [...images];
-  const [draggedImage] = reorderedImages.splice(dragIndex, 1);
-  reorderedImages.splice(dropIndex, 0, draggedImage);
-  setImages(reorderedImages);
-};
-
+  // ---------------------------------------------
+  // reordering functions manage here
+  const handleImageReorder = (dragIndex, dropIndex) => {
+    const reorderedImages = [...images];
+    const [draggedImage] = reorderedImages.splice(dragIndex, 1);
+    reorderedImages.splice(dropIndex, 0, draggedImage);
+    setImages(reorderedImages);
+  };
 
   return (
     <div className="px-2 md:px-10 mb-10">
@@ -76,9 +68,8 @@ const handleImageReorder = (dragIndex, dropIndex) => {
             <h1 className="text-xl">Gallery</h1>
           )}
         </nav>
-        
+
         <div className="grid grid-cols-2 md:grid-cols-5 gap-4 mx-2 md:mx-7">
-       
           {images.map((img, index) => (
             <div
               className={`border-2 border-gray-400 rounded-lg p-5 relative group cursor-pointer ${
@@ -93,19 +84,16 @@ const handleImageReorder = (dragIndex, dropIndex) => {
                 const dragIndex = e.dataTransfer.getData("text/plain");
                 handleImageReorder(parseInt(dragIndex), index);
               }}
-
             >
               {/* conditionally handle when image is selected */}
-
               <img
                 src={img}
                 alt=""
                 className={`max-w-full max-h-max  ${
                   selected.includes(index) ? "blur-[0.5px] " : ""
                 }`}
-                
               />
-
+               
               <input
                 type="checkbox"
                 className=" hidden absolute top-2 left-2 cursor-pointer group-hover:block h-7 w-5 checked:block"
@@ -120,9 +108,7 @@ const handleImageReorder = (dragIndex, dropIndex) => {
             src="/src/assets/images/addImage.jpg"
             alt=""
           />
-          
         </div>
-       
       </div>
     </div>
   );
